3ac32eea50ae005f66f1064fba979df851f566b3
VINODvoid <iamlearner.v@gmail.com>
2024-09-05 15:51:26 +0530

    chore: Add Multiply function and print name in 02basics.dart

---
+++ 02basics/02basics.dart
@@ -0,0 +1,15 @@
+int Multiply(int a , int b)
+{
+    return a*b;
+}
+void main()
+{
+  // Functions
+    void printName(String name)
+    {
+        print(name);
+    }
+    printName('John');
+    int sum =Multiply(2,3);
+    print(sum);
+}
