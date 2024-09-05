0b9590c6e070f8d0727e7e72c4dc24c2feeacc70
VINODvoid <iamlearner.v@gmail.com>
2024-09-05 15:04:18 +0530

    chore: Add control flows

---
+++ 01basics/03basics.dart
@@ -0,0 +1,17 @@
+void main()
+{
+    int age = 32;
+    if(age >= 18){
+        print('Adult');
+    }
+    else if(age == 32)
+    {
+        print('Adulthood');
+    }
+    else{
+        print('Child');
+    }
+
+
+    
+}
